ab5cf51df5b16888d5b5cd3bfc630507af620a22
Deepanshu Jain <deepanshu.jain@skilrock.com>
2018-12-23 18:25:56 +0530

    Added Auto scroll functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,24 @@
 var socket = io();
 
+
+function scrollToBottom() {
+	var messages = $('#messages');
+
+	var newMessage = messages.children('li:last-child');
+
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('connect', function() {
 	console.log('Connected to Server');
-
-
-
 });
 
 socket.on('disconnect' , function() {
@@ -25,6 +39,7 @@ socket.on('newMessage', function(message) {
 	//var li = $('<li></li>');
 	//li.text(`${message.from} ${formattedTime}: ${message.text}`);
 	$('#messages').append(html);
+	scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -45,6 +60,7 @@ socket.on('newLocationMessage', function(message) {
 	// a.attr('href', message.url);
 	// li.append(a);
 	$('#messages').append(html);
+	scrollToBottom();
 });
 
 $('#message-form').on('submit', function(e) {
